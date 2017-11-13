df4d5c2897b891d9120291de52f3aff14126316d
PharmyOf1 <phillip.harm@gmail.com>
2017-11-12 20:17:19 -0500

    added logo to header

--- static/assets/js/style.js
+++ static/assets/js/style.js
@@ -2,9 +2,13 @@ $(window).scroll(function() {
     var distanceFromTop = $(this).scrollTop();
     if (distanceFromTop >= $('.brand-main-top').height()) {
         $('.navbar').addClass('fixed');
-        $('.brand-main-top').css('margin-bottom','100px');
+        $('.navbar-nav').addClass('fixed');
+        $('.logo-nav-small').addClass('fixed');
+        $('.brand-main-top').css('margin-bottom','80px');
     } else {
         $('.navbar').removeClass('fixed');
+        $('.navbar-nav').removeClass('fixed');
+        $('.logo-nav-small').removeClass('fixed');
         $('.brand-main-top').css('margin-bottom','0px');
     }
 });
