76fa1ae69e0bd125d2b5b61923f9b1dbcaa79266
Phil Harm <pharm@apple.com>
2020-01-25 13:50:07 -0800

    preparing for dh

--- note_site/urls.py
+++ note_site/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('control/', admin.site.urls),
     path('', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
