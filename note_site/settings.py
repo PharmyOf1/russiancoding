25043e6889ce5404ab1a095f95d6f8ca1c0d1ae4
Phil Harm <pharm@apple.com>
2020-02-16 14:14:43 -0800

    footer change

--- note_site/settings.py
+++ note_site/settings.py
@@ -34,7 +34,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['russianandcoding.philharm.com','www.russianandcoding.philharm.com','localhost','127.0.0.1']
 
